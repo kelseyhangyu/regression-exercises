27004109aaeb48557964362e31051b5b7f301103
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-12-14 15:32:05 -0600

    finished exercises

---
+++ import.py
@@ -0,0 +1,17 @@
+def import_stuff():
+    # standard imports
+    # visualization
+    import matplotlib.pyplot as plt
+    # tabular data:
+    import numpy as np
+    import pandas as pd
+    # info:
+    import pydataset
+    # viz:
+    import seaborn as sns
+    # KNN, a distance based classifier
+    from sklearn.neighbors import KNeighborsClassifier
+    # scaling objects:
+    from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+    import warnings
+    warnings.filterwarnings('ignore')
